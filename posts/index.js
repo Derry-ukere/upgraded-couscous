00d67461df55958778ac680adf32e47cb2f036ab
derryukeree <ukderry@gmail.com>
2022-04-22 11:30:06 +0100

    clean up

--- posts/index.js
+++ posts/index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 
 // Get maximum character from ENVs else return 5 character
 const MAX_STAR_WARS_CHARACTERS = process.env.MAX_STAR_WARS_CHARACTERS || 5;
+const CRON_JOB = process.env.MAX_STAR_WARS_CRON_JOB;
+
 const config = {
   dictionaries: [starWars]
 }
@@ -16,11 +18,13 @@ for (let i = 1; i <= MAX_STAR_WARS_CHARACTERS; i += 1) {
  }
  return characterNames;
 };
+  if (CRON_JOB){
+    console.log('CRON_JOB_PARAMS :', CRON_JOB)
+  }
 app.get('/', (req, res) => {
-  console.log( 'MAX_STAR_WARS_CHARACTERS',MAX_STAR_WARS_CHARACTERS);
-  console.log( 'ENV',process.env.MAX_STAR_WARS_CHARACTERS );
  res.json(getStarWarsCharacters());
 });
-app.listen(3000, () => {
- console.log('Server started on port 3000');
+const port = 3000
+app.listen(port, () => {
+ console.log('Server started on port ',port);
 });
